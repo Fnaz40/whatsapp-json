5037080d2ef382b687a5cb513f15bb5c6c0e5505
Fnaz40 <48803891+Fnaz40@users.noreply.github.com>
2019-07-11 16:54:39 +0500

    WIP

--- script.js
+++ script.js
@@ -18,4 +18,22 @@ let details = [
         "profile": "https://www.google.com/search?q=image&rlz=1C1GCEU_enPK825PK827&tbm=isch&source=iu&ictx=1&fir=CJwabmfmxl2ySM%253A%252CpFs_4Fcq5AgpmM%252C_&vet=1&usg=AI4_-kSmsJSXmOnLPc_7R6JjVOmyfiRQpw&sa=X&ved=2ahUKEwiyspPayKzjAhXJzqQKHanBAagQ9QEwAHoECAQQBA#imgrc=2q-RPRnQqSH6YM:&vet=1",
         "messege": "jsdhksjd"
     },
+    {
+        "name": "faiza",
+        "contact": "034689767",
+        "profile": "https://www.google.com/search?q=image&rlz=1C1GCEU_enPK825PK827&tbm=isch&source=iu&ictx=1&fir=CJwabmfmxl2ySM%253A%252CpFs_4Fcq5AgpmM%252C_&vet=1&usg=AI4_-kSmsJSXmOnLPc_7R6JjVOmyfiRQpw&sa=X&ved=2ahUKEwiyspPayKzjAhXJzqQKHanBAagQ9QEwAHoECAQQBA#imgrc=cMDWvPzZurThEM:&vet=1",
+        "massege": "karachi is big city."
+    },
+    {
+        "name": "ALi Raza",
+        "contact": "031523503534",
+        "profile": "https://www.google.com/search?rlz=1C1GCEU_enPK825PK827&tbm=isch&sa=1&ei=ByEnXZbdGJaHwPAP_fyPwAo&q=programmer+images&oq=programmer+im&gs_l=img.1.0.0l3j0i5i30l2j0i8i30l3j0i8i10i30j0i8i30.25854.55781..63597...1.0..0.276.2694.0j12j2......0....1..gws-wiz-img.....0..0i67j0i10i24.NYKDfUZwMNY#imgrc=xn0oi-WCfVd3IM:",
+        "massege": "yes"
+    },
+    {
+        "name": "Sofia",
+        "contact": ["0342672176", "035879556"],
+        "profile": "https://www.google.com/search?q=image&rlz=1C1GCEU_enPK825PK827&tbm=isch&source=iu&ictx=1&fir=CJwabmfmxl2ySM%253A%252CpFs_4Fcq5AgpmM%252C_&vet=1&usg=AI4_-kSmsJSXmOnLPc_7R6JjVOmyfiRQpw&sa=X&ved=2ahUKEwiyspPayKzjAhXJzqQKHanBAagQ9QEwAHoECAQQBA#imgrc=CJwabmfmxl2ySM:&vet=1",
+        "massege": "Salaam"
+    }
 ]
