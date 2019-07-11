07ec45a5f8418ccaa89442d81a43f3a2f71e584e
Fnaz40 <48803891+Fnaz40@users.noreply.github.com>
2019-07-11 15:25:39 +0500

    json

---
+++ script.js
@@ -0,0 +1,20 @@
+// whatsapp data base.
+let details = [{
+    "name": "Bushra",
+    "contect" : ["03456778", "023344454"],
+    "profile" : "https://www.google.com/search?q=image&rlz=1C1GCEU_enPK825PK827&tbm=isch&source=iu&ictx=1&fir=CJwabmfmxl2ySM%253A%252CpFs_4Fcq5AgpmM%252C_&vet=1&usg=AI4_-kSmsJSXmOnLPc_7R6JjVOmyfiRQpw&sa=X&ved=2ahUKEwiyspPayKzjAhXJzqQKHanBAagQ9QEwAHoECAQQBA#imgrc=CJwabmfmxl2ySM:",
+    "massege": "Hello"
+    },
+    {
+        "name": "Fabeeha",
+        "contact": "03454656",
+        "profile": "https://www.google.com/search?q=image&rlz=1C1GCEU_enPK825PK827&tbm=isch&source=iu&ictx=1&fir=CJwabmfmxl2ySM%253A%252CpFs_4Fcq5AgpmM%252C_&vet=1&usg=AI4_-kSmsJSXmOnLPc_7R6JjVOmyfiRQpw&sa=X&ved=2ahUKEwiyspPayKzjAhXJzqQKHanBAagQ9QEwAHoECAQQBA#imgrc=1rz1N3TbXNAKgM:&vet=1",
+        "massege" : "hy"
+    },
+    {
+        "name": "sana",
+        "contact": "0346678",
+        "profile": "https://www.google.com/search?q=image&rlz=1C1GCEU_enPK825PK827&tbm=isch&source=iu&ictx=1&fir=CJwabmfmxl2ySM%253A%252CpFs_4Fcq5AgpmM%252C_&vet=1&usg=AI4_-kSmsJSXmOnLPc_7R6JjVOmyfiRQpw&sa=X&ved=2ahUKEwiyspPayKzjAhXJzqQKHanBAagQ9QEwAHoECAQQBA#imgrc=2q-RPRnQqSH6YM:&vet=1",
+        "messege": "jsdhksjd"
+    },
+]
